dc931c547125a6adcdd61c76a840c1dc14ed8f17
yannick tindy <yannicktindy@gmail.com>
2024-10-08 21:37:42 +0200

    show olympics datas on home page

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,31 +1,67 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http'; // Importation du service HttpClient pour les requêtes HTTP
+import { Injectable } from '@angular/core'; // Importation du décorateur Injectable pour créer des services
+import { BehaviorSubject, Observable } from 'rxjs'; // Importation de BehaviorSubject et Observable de RxJS pour la gestion des flux de données
+import { catchError, tap } from 'rxjs/operators'; // Importation des opérateurs RxJS pour le traitement des observables
+import { Olympics } from '../models/Olympic'; // Importation du modèle de données Olympics
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root', // Indique que le service est disponible dans toute l'application
 })
 export class OlympicService {
-  private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+  private olympicUrl = './assets/mock/olympic.json'; // Chemin vers le fichier JSON contenant les données olympiques
+  private olympics$ = new BehaviorSubject<Olympics[] | null>(null); // BehaviorSubject pour émettre les données, initialisé à null
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {} // Injection du service HttpClient dans le constructeur
 
-  loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
-      tap((value) => this.olympics$.next(value)),
-      catchError((error, caught) => {
-        // TODO: improve error handling
-        console.error(error);
-        // can be useful to end loading state and let the user know something went wrong
-        this.olympics$.next(null);
-        return caught;
+  loadInitialData(): Observable<Olympics[]> {
+    // Méthode pour charger les données depuis le fichier JSON
+    return this.http.get<Olympics[]>(this.olympicUrl).pipe( // Effectue une requête GET pour récupérer les données
+      tap((data) => this.olympics$.next(data)), // Émet les données chargées dans le BehaviorSubject
+      catchError((error) => { // Gestion des erreurs lors de la requête
+        console.error('Erreur lors du chargement des données olympiques:', error); // Affiche l'erreur dans la console
+        this.olympics$.next(null); // Émet null en cas d'erreur
+        throw error; // Relance l'erreur pour une gestion ultérieure
       })
     );
   }
 
-  getOlympics() {
-    return this.olympics$.asObservable();
+  getOlympics(): Observable<Olympics[] | null> {
+    // Méthode pour obtenir un observable des données olympiques
+    return this.olympics$.asObservable(); // Retourne l'instance BehaviorSubject sous forme d'Observable
   }
 }
+
+
+
+
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { BehaviorSubject } from 'rxjs';
+// import { catchError, tap } from 'rxjs/operators';
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class OlympicService {
+//   private olympicUrl = './assets/mock/olympic.json';
+//   private olympics$ = new BehaviorSubject<any>(undefined);
+
+//   constructor(private http: HttpClient) {}
+
+//   loadInitialData() {
+//     return this.http.get<any>(this.olympicUrl).pipe(
+//       tap((value) => this.olympics$.next(value)),
+//       catchError((error, caught) => {
+//         // TODO: improve error handling
+//         console.error(error);
+//         // can be useful to end loading state and let the user know something went wrong
+//         this.olympics$.next(null);
+//         return caught;
+//       })
+//     );
+//   }
+
+//   getOlympics() {
+//     return this.olympics$.asObservable();
+//   }
+// }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
+// src/app/pages/home/home.component.ts
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { Olympics } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -8,11 +10,39 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+  public olympics$: Observable<Olympics[] | null>; // Déclarez l'Observable pour les données
 
-  constructor(private olympicService: OlympicService) {}
+  constructor(private olympicService: OlympicService) {
+    this.olympics$ = this.olympicService.getOlympics(); // Initialisez l'Observable
+  }
 
   ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
+    this.olympicService.loadInitialData().subscribe(
+      () => {
+        // Les données sont déjà émises par le BehaviorSubject
+      },
+      (error) => {
+        console.error('Erreur lors du chargement des données olympiques:', error);
+      }
+    );
   }
 }
+
+// import { Component, OnInit } from '@angular/core';
+// import { Observable, of } from 'rxjs';
+// import { OlympicService } from 'src/app/core/services/olympic.service';
+
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: './home.component.html',
+//   styleUrls: ['./home.component.scss'],
+// })
+// export class HomeComponent implements OnInit {
+//   public olympics$: Observable<any> = of(null);
+
+//   constructor(private olympicService: OlympicService) {}
+
+//   ngOnInit(): void {
+//     this.olympics$ = this.olympicService.getOlympics();
+//   }
+// }

bb48aec803a1bdfad04780befb3fd578f9f5b41b
yannick tindy <yannicktindy@gmail.com>
2024-10-15 15:34:02 +0200

    clean spacing and add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts'; // Importez le module NgxCharts
 import { DetailComponent } from './pages/detail/detail.component';
+import { HeaderComponent } from './partials/header/header.component';
 
 @NgModule({
   declarations: [AppComponent, DetailComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, NgxChartsModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, NgxChartsModule, HeaderComponent, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
